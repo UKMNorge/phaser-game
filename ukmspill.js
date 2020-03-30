38a87231a176578fd2a68c10b73f5c0db581f5b3
Marius Mandal <mariusmandal@gmail.com>
2020-03-30 14:42:03 +0200

    riktig path

--- ukmspill.js
+++ ukmspill.js
@@ -39,11 +39,11 @@ var scriptSource = (function() {
 }()).match(/.*\//)[0];
 
 function preload() {
-    this.load.image('sky', scriptSource + '/assets/sky.png');
-    this.load.image('ground', scriptSource + '/assets/platform.png');
-    this.load.image('star', scriptSource + '/assets/star.png');
-    this.load.image('bomb', scriptSource + '/assets/bomb.png');
-    this.load.spritesheet('dude', scriptSource + '/assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+    this.load.image('sky', scriptSource + 'assets/sky.png');
+    this.load.image('ground', scriptSource + 'assets/platform.png');
+    this.load.image('star', scriptSource + 'assets/star.png');
+    this.load.image('bomb', scriptSource + 'assets/bomb.png');
+    this.load.spritesheet('dude', scriptSource + 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
 }
 
 function create() {
